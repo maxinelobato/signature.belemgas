a9e589e4445e67f88be5e0169b3b9adc0be2a787
maxinelobato <maxinelobato89@hotmail.com>
2022-06-21 15:19:58 -0300

    fix-bgbelemgas

--- src/pages/components/BgBelemGas.tsx
+++ src/pages/components/BgBelemGas.tsx
@@ -1,11 +1,9 @@
 import { Box, BoxProps } from "@chakra-ui/react";
 
-export function BgBelemGas(props: BoxProps) {
-  return (
-    <Box
-      {...props}
-      minH="100vh"
-      bgGradient="linear(to-r, brandBelemGas.orange500, brandBelemGas.yellow300, brandBelemGas.orange500)"
-    />
-  );
-}
+export const BgBelemGas = (props: BoxProps) => (
+  <Box
+    {...props}
+    minH="100vh"
+    bgGradient="linear(to-r, brandBelemGas.orange500, brandBelemGas.yellow300, brandBelemGas.orange500)"
+  />
+);
