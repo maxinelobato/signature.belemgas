27b0712eaae94c63c2ed2a23a228dba21d2b7ac8
maxinelobato <maxinelobato89@hotmail.com>
2022-06-22 19:40:36 -0300

    fix-theme-dark

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,27 +2,26 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { extendTheme } from "@chakra-ui/react";
 
-const colors = {
-  brandBelemGas: {
-    orange100: "#FEEBC8",
-    orange500: "#DD6B20",
-    yellow300: "#F6E05E",
-    yellow500: "#D69E2E",
-    red500: "#E53E3E",
-    green600: "#2F855A",
-    green200: "#9AE6B4",
-    blue200: "#90CDF4",
-    gray900: "#171923",
-    gray700: "#2D3748",
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: true,
   },
-};
-
-const config = {
-  initialColorMode: "dark",
-  useSystemColorMode: true,
-};
-
-const theme = extendTheme({ colors, config });
+  colors: {
+    brandBelemGas: {
+      orange100: "#FEEBC8",
+      orange500: "#DD6B20",
+      yellow300: "#F6E05E",
+      yellow500: "#D69E2E",
+      red500: "#E53E3E",
+      green600: "#2F855A",
+      green200: "#9AE6B4",
+      blue200: "#90CDF4",
+      gray900: "#171923",
+      gray700: "#2D3748",
+    },
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
